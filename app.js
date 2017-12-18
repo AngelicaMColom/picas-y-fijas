4b630447235c6cc7090b664d090567d282cdaff8
Angelica Maria Duque <mduque@hugeinc.com>
2017-12-18 00:58:00 -0500

    added body table, added css variable

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // select element to be listen
-const ObjGenerals = {
+const OBJGEN = {
   digitalCounter: 4,
   counter: 0,
   containerTitleActive: "container__title__suptitle_active",
   addTable: document.querySelector(".container__body__tablet"),
+  addBodyTable: document.querySelector("tbody"),
+  baseHige: 600,
   minValueRandom: 1000,
   maxValueRandom: 9999
 }
@@ -29,42 +31,35 @@ function submitActions(e) {
   inputVal.value = "";
 }
 
-// valide input and add cointer
+/**
+ *  @param: {object} inputsObject
+ **/
 function conditionalValidation(inputsObject) {
   let valueString = inputsObject.value.toString();
   compareValueOfString(valueString) || compareValueInput(inputsObject) ? addUserError() : remuveUserErrror();
-  !compareValueOfString(valueString) && compareValueCount(inputsObject) ? ObjGenerals.counter += 1 : ObjGenerals.counter;
-  inputsObject.valueCount && ObjGenerals.counter !== 0 ? convertNumbers(valueString) : ObjGenerals.counter;
-}
-
-function compareValueInput(inputsObject) {
-
-  return !inputsObject.value || inputsObject.value.includes(" ") ||
-    inputsObject.valueCount < ObjGenerals.digitalCounter ||
-    inputsObject.valueCount > ObjGenerals.digitalCounter;
-}
-
-function compareValueCount(inputsObject) {
-
-  return inputsObject.enter && inputsObject.valueCount === ObjGenerals.digitalCounter;
+  !compareValueOfString(valueString) && compareValueCount(inputsObject) ? OBJGEN.counter += 1 : OBJGEN.counter;
+  inputsObject.valueCount && OBJGEN.counter !== 0 && compareValueCount(inputsObject) ? compareUserNumber(valueString) : OBJGEN.counter;
 }
 
 /**
  * @param: {number} userNumber input value and count which is initiated
- */
-
-function convertNumbers(userNumber) {
-  console.log(userNumber, numberRandom);
-  if(userNumber === numberRandom) {
-    ObjGenerals.counter = 0;
+ **/
+function compareUserNumber(userNumber) {
+  if (OBJGEN.counter === 1) {
     numberRandom = generateNumberRandomNumber();
+  }
+  if(userNumber === numberRandom) {
+    OBJGEN.counter = 0;
     winnerUser();
   } else {
     loserUser(userNumber, numberRandom);
-    createTableHead();
+    displayTableHead();
   }
 }
 
+/**
+ * @param: {number} userNumber,numberRandom
+ **/
 function loserUser(userNumber, numberRandom) {
   let score = {
     userNumber,
@@ -81,56 +76,97 @@ function loserUser(userNumber, numberRandom) {
   createTableBody(score);
 }
 
-function createTableHead() {
-  ObjGenerals.addTable.style.display = "block";
+/**
+ * display table
+ **/
+function displayTableHead() {
+  OBJGEN.addTable.style.display = "block";
 }
 
+/**
+ * create bodytable
+ * @param:{object} score
+ **/
 function createTableBody(score) {
-  for (let i = 1; i < 4; i++){
-      let tr = document.createElement('tr');
-
-      let td1 = document.createElement('td');
-      let td2 = document.createElement('td');
-
-      let text1 = document.createTextNode('Text1');
-      let text2 = document.createTextNode('Text2');
-
-      td1.appendChild(text1);
-      td2.appendChild(text2);
-      tr.appendChild(td1);
-      tr.appendChild(td2);
-
-      //table.appendChild(tr);
-  }
-
+  addheight();
+  let tr = document.createElement('tr');
+  Object.values(score).forEach(val => {
+    let td1 = document.createElement('td');
+    let texNode = document.createTextNode(val);
+    td1.appendChild(texNode);
+    tr.appendChild(td1);
+  })
+  OBJGEN.addBodyTable.appendChild(tr);
 }
 
-
-
+/**
+ * create RandomNumber
+ *  @returns: {number} numberRandom;
+ **/
 function generateNumberRandomNumber() {
-  let numberValue = getRandomInt(ObjGenerals.minValueRandom, ObjGenerals.maxValueRandom).toString();
+  let numberValue = getRandomInt(OBJGEN.minValueRandom, OBJGEN.maxValueRandom).toString();
   if(compareValueOfString(numberValue)) {
-    numberValue = getRandomInt(ObjGenerals.minValueRandom, ObjGenerals.maxValueRandom).toString();
+    numberValue = generateNumberRandomNumber();
   }
 
   return numberValue;
 }
 
+/**
+ * generated RandomNumber
+ *  @param:{number} minimun number and maximun number
+ *  @returns: {number} numberRandom;
+ */
 function getRandomInt(min, max) {
 
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-function compareValueOfString(num) {
+/**
+ * compare each value of the string
+ *  @param:{string} userNumber or numberRandom
+ *  @returns: {boolean}
+ **/
+function compareValueOfString(str) {
 
-  return num.split("")
+  return str.split("")
     .map(Number).sort()
     .some((val, i, arrayU) => val === arrayU[i + 1]);
 }
 
-let returnArray = (num) => { return num.split("").map(Number) };
-let addUserError = () => { addError.classList.add(ObjGenerals.containerTitleActive) };
-let remuveUserErrror = () => { addError.classList.remove(ObjGenerals.containerTitleActive) };
+/**
+ * compare value of the string given by the user
+ *  @param:{Object} inputsObject
+ *  @returns: {boolean}
+ **/
+
+function compareValueInput(inputsObject) {
+
+  return !inputsObject.value || inputsObject.value.includes(" ") ||
+    inputsObject.valueCount < OBJGEN.digitalCounter ||
+    inputsObject.valueCount > OBJGEN.digitalCounter;
+}
+/**
+ * compare value of the string given by the user
+ *  @param: {Object} inputsObject
+ *  @returns: {boolean}
+ **/
+function compareValueCount(inputsObject) {
+
+  return inputsObject.enter && inputsObject.valueCount === OBJGEN.digitalCounter;
+}
+
+function addheight() {
+  if (OBJGEN.counter > 3) {
+    const suffix =  'px';
+    let pxNumber = OBJGEN.baseHige += 50;
+         document.documentElement.style.setProperty(`--baseHige`, pxNumber + suffix);
+
+  }
+}
+let returnArray = (str) => { return str.split("").map(Number) };
+let addUserError = () => { addError.classList.add(OBJGEN.containerTitleActive) };
+let remuveUserErrror = () => { addError.classList.remove(OBJGEN.containerTitleActive) };
 let closeModule = () => { modal.style.display = "none"; }
 let winnerUser = () => { modal.style.display = "block"; }
 
